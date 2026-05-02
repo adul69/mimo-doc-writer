efd91d19eb5aa3510e83777e666f2fffe860be1d
adul69 <adul69@users.noreply.github.com>
2026-05-02 19:02:39 +0800

    feat: Add MiMo API key to WSGI config

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -1,11 +1,14 @@
 # WSGI configuration for PythonAnywhere
 import sys
+import os
 
 project_home = '/home/Adul69/mimo-doc-writer'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-import os
+# Set environment variables directly
+os.environ['MIMO_API_KEY'] = 'sk-sx5305wu8bb3usteoszw6yuhcl9474jmhxhp1vxef455rwyg'
+os.environ['MIMO_API_URL'] = 'https://api.xiaomimimo.com/v1'
 os.environ['FLASK_ENV'] = 'production'
 
 from app import app as application
